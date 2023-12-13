d68c3bd767e72b47b27607d84cd55cdf378b936d
MenaVillalobos <jimee69styles@gmail.com>
2023-12-13 01:16:06 -0600

    using Container and SX

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Button } from "@mui/material";
+import { Button, Container, colors, rgbToHex } from "@mui/material";
 
 const App = () => {
   return (
     <>
+      <Container maxWidth = 'xs' sx = {{border: 2, borderColor: '#1ABC9C'}}>
       <h1>My First App w/MUI</h1>
-      <Button variant="contained">1st MUI Button</Button>
+      <Button variant="contained" sx = {{bgcolor: '#04DAB1'}}>1st MUI Button</Button>
+      </Container>
     </>
   )
 }
