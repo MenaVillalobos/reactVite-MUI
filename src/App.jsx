10db667e9519cacb3da3224881a26db30c36b579
MenaVillalobos <jimee69styles@gmail.com>
2023-12-13 15:57:45 -0600

    Typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { Button, Container, colors, rgbToHex } from "@mui/material";
+import { Button, Container, Typography, colors, rgbToHex } from "@mui/material";
 
 const App = () => {
   return (
     <>
-      <Container maxWidth = 'xs' sx = {{border: 2, borderColor: '#1ABC9C'}}>
-      <h1>My First App w/MUI</h1>
-      <Button variant="contained" sx = {{bgcolor: '#04DAB1'}}>1st MUI Button</Button>
+      <Container maxWidth = 'xs' sx = {{ border: 2, borderColor: '#1ABC9C' }}>
+        <h1>My First App w/MUI</h1>
+        <Typography variant = 'h2' sx = {{ boxShadow: 3, marginBottom: 2 }} >
+          This is an h2 component
+        </Typography>
+        <Typography variant = 'h2' 
+        component = 'span' 
+        sx = {{ boxShadow: 3, marginBottom: 2, color: '#049CDA', display: 'flex', justifyContent: 'center' }} >
+          This is an h2 component that behaves like a paragraph
+        </Typography>
+        <Button variant="contained" sx = {{ bgcolor: '#04DAB1', marginBottom: 2, marginTop: 2 }}>
+          1st MUI Button
+        </Button>
       </Container>
     </>
   )
